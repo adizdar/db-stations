792be81c2dd820d76a2762dcdfd3dc032dccae17
adizdar <ahmed.dizdar@viison.com>
2020-11-23 18:24:20 +0100

    Add more stations

--- api/server/api/services/stations.db.service.js
+++ api/server/api/services/stations.db.service.js
@@ -21,6 +21,11 @@ class StationsDatabase {
         new Position(50.111942, 8.628701),
         new Position(50.112086, 8.628563),
       ],
+      [
+        new Position(50.110978, 8.620388),
+        new Position(50.110717, 8.620276),
+        new Position(50.110904, 8.621038),
+      ],
     ];
 
     const bicycles = [
@@ -28,16 +33,22 @@ class StationsDatabase {
         new Marker(new Position(50.115050, 8.625609)), 
         new Marker(new Position(50.115056, 8.626041)),
         new Marker(new Position(50.115073, 8.625684)),
+        new Marker(new Position(50.114997, 8.625832)),
       ],
       [
         new Marker(new Position(50.112043, 8.628528)), 
         new Marker(new Position(50.112000, 8.628498)),
         new Marker(new Position(50.111991, 8.628452)),
-      ]
+      ],
+      [
+        new Marker(new Position(50.110882, 8.620826)), 
+        new Marker(new Position(50.110871, 8.620608)),
+      ],
     ];
 
     this.insert(stations[0], bicycles[0]);
     this.insert(stations[1], bicycles[1]);
+    this.insert(stations[2], bicycles[2]);
   }
 
   all() {
